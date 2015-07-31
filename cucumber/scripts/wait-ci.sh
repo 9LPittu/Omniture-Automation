21a438e4764b3129fa3b47439487afa02349adc3
Ulises Pulido <ulises.pulido.cft@jcrew.com>
2015-07-31 10:52:40 -0400

    Refactoring to make code more understandable.

--- cucumber/scripts/wait-ci.sh
+++ cucumber/scripts/wait-ci.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/env bash
 function waitForServerRestart {
 
-	COUNTER=0
+	RETRY_NUMBER=0
+	MAX_NUMBER_OF_RETRIES=18
 	echo 'Waiting for server to restart'
-	until $(curl --output /dev/null --silent --head --fail http://www.sidecar-brn-ci.jcrew.com); do
-		if [ $COUNTER -gt 18 ]; then
+	until $(curl --output /dev/null --silent --head --fail http://www.sidecar-brn-.jcrew.com); do
+		if [ ${RETRY_NUMBER} -ge ${MAX_NUMBER_OF_RETRIES} ]; then
 			echo 'Server startup timed out'
 			return 1
 		else 
-			((COUNTER += 1)) 
-			echo Retry $COUNTER
+			((RETRY_NUMBER += 1))
+			echo Retry ${RETRY_NUMBER}
 		 fi
 		sleep 10
 	done
