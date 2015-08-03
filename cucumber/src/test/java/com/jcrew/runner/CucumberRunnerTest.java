895c549b2d21cfcb1d4e5223c9ecbf8a5d20d85a
Ulises Pulido <ulises.pulido.cft@jcrew.com>
2015-08-03 11:55:08 -0400

    [US13022] Adding reporting capabilities to builds.

--- cucumber/src/test/java/com/jcrew/runner/CucumberRunnerTest.java
+++ cucumber/src/test/java/com/jcrew/runner/CucumberRunnerTest.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "features"
-        ,glue={"com.jcrew.steps"}
+        features = "features",
+        glue = {"com.jcrew.steps"},
+        format = {"pretty", "html:target/cucumber", "json:target/cucumber-report.json"}
 )
 public class CucumberRunnerTest {
 }
