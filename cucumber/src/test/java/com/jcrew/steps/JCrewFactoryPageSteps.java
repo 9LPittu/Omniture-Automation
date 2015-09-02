97e6f89281b6c940904c9379ffbfbc96457006b8
unknown <9hvenaga@770-DG-16133.jcrew.com>
2015-09-02 09:14:45 -0400

    [TA31752] Adding verification of footer links presence.

---
+++ cucumber/src/test/java/com/jcrew/steps/JCrewFactoryPageSteps.java
@@ -0,0 +1,19 @@
+package com.jcrew.steps;
+
+import com.jcrew.page.JcrewFactoryPage;
+import com.jcrew.util.DriverFactory;
+import cucumber.api.java.en.And;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by 9hvenaga on 9/1/2015.
+ */
+public class JCrewFactoryPageSteps extends DriverFactory {
+
+    @And("^Verify user is on the j.crew factory page$")
+    public void verify_user_is_on_the_j_crew_factory_page() throws Throwable {
+        JcrewFactoryPage jCrewFactoryPage = new JcrewFactoryPage(getDriver());
+        assertTrue("User should be in jcrew factory page", jCrewFactoryPage.isJcrewFactoryPage());
+    }
+}
