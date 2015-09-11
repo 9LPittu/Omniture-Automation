12b3a94ca5fd34246acecc9a99faf418730be349
Ulises Pulido <ulises.pulido.cft@jcrew.com>
2015-09-11 11:04:35 -0400

    [TA31768] Adding verification for Shipping And Handling Page and Contact Us about having embedded header and footer working

---
+++ cucumber/src/test/java/com/jcrew/runner/qa/EmbeddedHeaderFooterShippingAndHandlingPageRunnerTest.java
@@ -0,0 +1,19 @@
+package com.jcrew.runner.qa;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "features/qa",
+        tags = {"@EmbeddedHeaderFooterShippingAndHandlingPage"},
+        glue = {"com.jcrew.steps"},
+        format = {
+                "json:target/cucumber-embeddedheaderfooterqashippingandhandlingpagefeatures.json",
+                "pretty",
+                "html:target/cucumber/embeddedheaderfooterqashippingandhandlingpagefeatures"
+        }
+)
+public class EmbeddedHeaderFooterShippingAndHandlingPageRunnerTest {
+}
