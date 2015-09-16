b03f67055bb8227e7cb94906c5d2d37682a43d46
Ulises Pulido <ulises.pulido.cft@jcrew.com>
2015-09-16 10:57:18 -0400

    Created profile for executing all tests but Embedded Header Footer

--- cucumber/src/test/java/com/jcrew/runner/qa/AccountFeaturesRunnerTest.java
+++ cucumber/src/test/java/com/jcrew/runner/qa/AccountFeaturesRunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "features/qa",
-        tags = {"@VerifySignin"},
+        tags = {"@Account"},
         glue = {"com.jcrew.steps"},
         format = {
                 "json:target/cucumber-accountqafeatures.json",
