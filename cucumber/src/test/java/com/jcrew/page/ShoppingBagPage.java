2dcd229abd23f555b0cbfc9d3c479c3928cf3006
Ulises Pulido <ulises.pulido.cft@jcrew.com>
2015-09-29 11:55:50 -0400

    [TA33538] Verification using random products up to shopping bag page.

--- cucumber/src/test/java/com/jcrew/page/ShoppingBagPage.java
+++ cucumber/src/test/java/com/jcrew/page/ShoppingBagPage.java
@@ -17,101 +17,107 @@ import org.slf4j.LoggerFactory;
 
 public class ShoppingBagPage {
 
-    private final Logger logger = LoggerFactory.getLogger(ShoppingBagPage.class);
+	private final Logger logger = LoggerFactory.getLogger(ShoppingBagPage.class);
 
-    private final WebDriver driver;
-    @FindBy(id = "button-checkout")
-    private WebElement checkoutLink;
+	private final WebDriver driver;
+	@FindBy(id = "button-checkout")
+	private WebElement checkoutLink;
 
-    @FindBy(css = ".item-actions > .item-edit")
-    private WebElement editAction;
+	@FindBy(css = ".item-actions > .item-edit")
+	private WebElement editAction;
 
-    @FindBy(css = ".item-actions > .item-remove")
-    private WebElement removeAction;
+	@FindBy(css = ".item-actions > .item-remove")
+	private WebElement removeAction;
 
-    @FindBy(css = ".item-group-price >.item-total")
-    private WebElement itemTotal;
+	@FindBy(css = ".item-group-price >.item-total")
+	private WebElement itemTotal;
 
-    @FindBy(css = ".summary-item > .summary-value")
-    private WebElement subtotalValue;
+	@FindBy(css = ".summary-item > .summary-value")
+	private WebElement subtotalValue;
 
-    @FindBy(id = "checkout")
-    private WebElement articleCheckout;
+	@FindBy(id = "checkout")
+	private WebElement articleCheckout;
 
-    @FindBy(className = "item-product")
-    private WebElement itemProductSection;
+	@FindBy(className = "item-product")
+	private WebElement itemProductSection;
 
-    @FindBy(className = "item-qty")
-    private WebElement itemQuantity;
+	@FindBy(className = "item-qty")
+	private WebElement itemQuantity;
 
-    public ShoppingBagPage(WebDriver driver) {
-        this.driver = driver;
-        PageFactory.initElements(this.driver, this);
-    }
+	public ShoppingBagPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(this.driver, this);
+	}
 
 
-    public void click_checkout_button() {
+	public void click_checkout_button() {
 
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(checkoutLink));
+		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(checkoutLink));
 
-        checkoutLink.click();
+		checkoutLink.click();
 
-    }
+	}
 
-    public boolean isEditButtonPresent() {
-        return editAction.isDisplayed();
-    }
+	public boolean isEditButtonPresent() {
+		return editAction.isDisplayed();
+	}
 
-    public boolean isRemoveButtonPresent() {
-        return removeAction.isDisplayed();
-    }
+	public boolean isRemoveButtonPresent() {
+		return removeAction.isDisplayed();
+	}
 
-    public String getTotalAmountPage() {
-        return itemTotal.getText();
-    }
+	public String getTotalAmountPage() {
+		return itemTotal.getText();
+	}
 
-    public String getSubtotalValue() {
-        return subtotalValue.getText();
-    }
+	public String getSubtotalValue() {
+		return subtotalValue.getText();
+	}
 
-    public boolean isArticleCheckoutPresent() {
-        return articleCheckout.isDisplayed();
-    }
+	public boolean isArticleCheckoutPresent() {
+		return articleCheckout.isDisplayed();
+	}
 
-    public void click_edit_button() {
-        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(editAction));
-        editAction.click();
-    }
+	public void click_edit_button() {
+		new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(editAction));
+		editAction.click();
+	}
 
-    public boolean isProductColorDisplayed(String productColor) {
-        WebElement productColorElement = getProductElementValue(productColor);
+	public boolean isProductColorDisplayed(String productColor) {
+		WebElement productColorElement = getProductElementValue(productColor);
 
-        return productColorElement.isDisplayed();
-    }
+		return productColorElement.isDisplayed();
+	}
 
-    private WebElement getProductElementValue(String element) {
-        return getDescriptionElementFor(ExpectedConditions.visibilityOf(
-                itemProductSection.findElement(By.xpath(".//span[contains(@class, 'notranslate') and " +
-                        "text()='" + element + "']"))));
-    }
+	private WebElement getProductElementValue(String element) {
+		return getDescriptionElementFor(ExpectedConditions.visibilityOf(
+				itemProductSection.findElement(By.xpath(".//span[contains(@class, 'notranslate') and " +
+						"text()='" + element + "']"))));
+	}
 
-    private WebElement getDescriptionElementFor(ExpectedCondition<WebElement> isTrue) {
-        return new WebDriverWait(driver, 10).until(
-                isTrue);
-    }
+	private WebElement getDescriptionElementFor(ExpectedCondition<WebElement> isTrue) {
+		return new WebDriverWait(driver, 10).until(
+				isTrue);
+	}
 
-    public boolean isProductSizeDisplayed(String productSize) {
-        WebElement productSizeElement = getProductElementValue(productSize);
-        return productSizeElement.isDisplayed();
-    }
+	public boolean isProductSizeDisplayed(String productSize) {
+		WebElement productSizeElement = getProductElementValue(productSize);
+		return productSizeElement.isDisplayed();
+	}
 
-    public String getItemQuantity() {
-        Select select = new Select(itemQuantity);
-        WebElement selectedOption = select.getFirstSelectedOption();
-        return selectedOption.getText();
-    }
-    
-    public void verifyMobilePageDisplayed(){
-    	assertTrue("Mobile page should be displayed", checkoutLink.isDisplayed());
-    }
+	public String getItemQuantity() {
+		Select select = new Select(itemQuantity);
+		WebElement selectedOption = select.getFirstSelectedOption();
+		return selectedOption.getText();
+	}
+
+
+	public String getProductName() {
+		return itemProductSection.findElement(By.className("item-name")).getText();
+	}
+
+
+	public void verifyMobilePageDisplayed() {
+		assertTrue("Mobile page should be displayed", checkoutLink.isDisplayed());
+	}	
 }
--- cucumber/src/test/java/com/jcrew/steps/ShoppingBagSteps.java
+++ cucumber/src/test/java/com/jcrew/steps/ShoppingBagSteps.java
@@ -2,6 +2,7 @@ package com.jcrew.steps;
 
 import com.jcrew.page.ShoppingBagPage;
 import com.jcrew.util.DriverFactory;
+import com.jcrew.util.StateHolder;
 import com.jcrew.util.Reporting;
 
 import cucumber.api.Scenario;
@@ -21,107 +22,145 @@ import static org.junit.Assert.assertTrue;
 public class ShoppingBagSteps extends DriverFactory {
 
 
-    private final ShoppingBagPage shoppingBagPage = new ShoppingBagPage(getDriver());
-    
-    private Scenario scenario;
-    private Reporting reporting = new Reporting();
-    
-    @Before
-    public void getScenarioObject(Scenario s){
-      this.scenario = s;
-    }
+	private final ShoppingBagPage shoppingBagPage = new ShoppingBagPage(getDriver());
+	private StateHolder stateHolder = StateHolder.getInstance();
+	private Scenario scenario;
+	private Reporting reporting = new Reporting();
 
-    @When("^Clicks on checkout$")
-    public void clicks_on_checkout() throws Throwable {
-        shoppingBagPage.click_checkout_button();
-        
-        reporting.takeScreenshot(scenario);
-    }
+	@Before
+	public void getScenarioObject(Scenario s){
+		this.scenario = s;
+	}
 
-    @Then("^User should be in shopping bag page$")
-    public void user_should_be_in_shopping_bag_page() throws Throwable {
-        assertTrue("Article checkout should have been present",
-                shoppingBagPage.isArticleCheckoutPresent());
-    }
+	@When("^Clicks on checkout$")
+	public void clicks_on_checkout() throws Throwable {
+		shoppingBagPage.click_checkout_button();
 
-    @And("^Verifies edit button is present$")
-    public void verifies_edit_button_is_present() throws Throwable {
-        assertTrue(shoppingBagPage.isEditButtonPresent());
-        
-        reporting.takeScreenshot(scenario);
-    }
+		reporting.takeScreenshot(scenario);
+	}
 
-    @And("^Verifies remove button is present$")
-    public void verifies_remove_button_is_present() throws Throwable {
-        assertTrue(shoppingBagPage.isRemoveButtonPresent());
-        
-        reporting.takeScreenshot(scenario);
-    }
+	@Then("^User should be in shopping bag page$")
+	public void user_should_be_in_shopping_bag_page() throws Throwable {
+		assertTrue("Article checkout should have been present",
+				shoppingBagPage.isArticleCheckoutPresent());
+	}
 
-    @And("^Verifies that total amount and subtotal values are similar$")
-    public void verifies_that_total_amount_and_subtotal_values_are_similar() throws Throwable {
-        String totalAmount = shoppingBagPage.getTotalAmountPage();
-        String subtotalValue = shoppingBagPage.getSubtotalValue();
-        assertEquals("Total Amount and subtotal should be the same", totalAmount, subtotalValue);
-    }
+	@And("^Verifies edit button is present$")
+	public void verifies_edit_button_is_present() throws Throwable {
+		assertTrue(shoppingBagPage.isEditButtonPresent());
 
-    @Given("^Verifies that total amount and subtotal values are numbers$")
-    public void Verifies_that_total_amount_and_subtotal_values_are_numbers() throws Throwable {
-        String totalAmount = shoppingBagPage.getTotalAmountPage();
-        String subtotalValue = shoppingBagPage.getSubtotalValue();
-        Pattern totalAmountPattern = Pattern.compile("^\\$\\d(\\d)?\\.\\d\\d$");
-        Pattern subtotalAmountPattern = Pattern.compile("^\\$\\d{1,3}(,\\d{3}|\\d)?\\.\\d\\d$");
+		reporting.takeScreenshot(scenario);
+	}
 
-        Matcher totalAmountMatch = totalAmountPattern.matcher(totalAmount);
-        Matcher subtotalAmountMatch = subtotalAmountPattern.matcher(subtotalValue);
+	@And("^Verifies remove button is present$")
+	public void verifies_remove_button_is_present() throws Throwable {
+		assertTrue(shoppingBagPage.isRemoveButtonPresent());
 
-        assertTrue("Total amount should be a price value", totalAmountMatch.matches());
-        assertTrue("Subtotal amount should be a price value", subtotalAmountMatch.matches());
+		reporting.takeScreenshot(scenario);
+	}
 
-    }
+	@And("^Verifies that total amount and subtotal values are similar$")
+	public void verifies_that_total_amount_and_subtotal_values_are_similar() throws Throwable {
+		String totalAmount = shoppingBagPage.getTotalAmountPage();
+		String subtotalValue = shoppingBagPage.getSubtotalValue();
+		assertEquals("Total Amount and subtotal should be the same", totalAmount, subtotalValue);
+	}
 
-    @And("^Clicks edit button on item bag page$")
-    public void clicks_edit_button_on_item_bag_page() throws Throwable {
-        shoppingBagPage.click_edit_button();
-    }
+	@Given("^Verifies that total amount and subtotal values are numbers$")
+	public void Verifies_that_total_amount_and_subtotal_values_are_numbers() throws Throwable {
+		String totalAmount = shoppingBagPage.getTotalAmountPage();
+		String subtotalValue = shoppingBagPage.getSubtotalValue();
+		Pattern totalAmountPattern = Pattern.compile("^\\$\\d(\\d)?\\.\\d\\d$");
+		Pattern subtotalAmountPattern = Pattern.compile("^\\$\\d{1,3}(,\\d{3}|\\d)?\\.\\d\\d$");
 
-    @Then("^Verify color ([^\"]*) is displayed in shopping bag$")
-    public void verify_color_is_displayed_in_shopping_bag(String productColor) throws Throwable {
-        assertTrue(productColor + " color should have been displayed",
-                shoppingBagPage.isProductColorDisplayed(productColor));
-    }
+		Matcher totalAmountMatch = totalAmountPattern.matcher(totalAmount);
+		Matcher subtotalAmountMatch = subtotalAmountPattern.matcher(subtotalValue);
 
-    @And("^Verify size ([^\"]*) is displayed in shopping bag$")
-    public void verify_size_is_displayed_in_shopping_bag(String productSize) throws Throwable {
-        assertTrue(productSize + " color should have been displayed",
-                shoppingBagPage.isProductSizeDisplayed(productSize));
-    }
+		assertTrue("Total amount should be a price value", totalAmountMatch.matches());
+		assertTrue("Subtotal amount should be a price value", subtotalAmountMatch.matches());
 
-    @And("^Verify ([^\"]*) items are specified as quantity in shopping bag$")
-    public void verify_items_are_specified_as_quantity(String productQuantity) throws Throwable {
-        assertEquals("Product Quantity should be the same", productQuantity,
-                shoppingBagPage.getItemQuantity());
-    }
-    
-    @Then("change URL to mobile URL")
-    public void change_URL_To_Mobile_URL(){
-    	
-    	//Get the current URL and append the ?isMobile=true
-    	String currentURL = getDriver().getCurrentUrl().trim();
-    	String browserURL = currentURL.replaceAll("sidecar=true", "");
-    	browserURL = browserURL + "isMobile=true";
-    	
-    	//navigate to the mobile URL
-    	getDriver().navigate().to(browserURL);
-    	getDriver().manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    	
-    	reporting.takeScreenshot(scenario);
-    }
-    
-    @Then("verify mobile page is displayed")
-    public void verify_mobile_page_displayed(){
-    	shoppingBagPage.verifyMobilePageDisplayed();
-    	
-    	reporting.takeScreenshot(scenario);
-    }
+	}
+
+	@And("^Clicks edit button on item bag page$")
+	public void clicks_edit_button_on_item_bag_page() throws Throwable {
+		shoppingBagPage.click_edit_button();
+	}
+
+	@Then("^Verify color ([^\"]*) is displayed in shopping bag$")
+	public void verify_color_is_displayed_in_shopping_bag(String productColor) throws Throwable {
+		assertTrue(productColor + " color should have been displayed",
+				shoppingBagPage.isProductColorDisplayed(productColor));
+	}
+
+	@And("^Verify size ([^\"]*) is displayed in shopping bag$")
+	public void verify_size_is_displayed_in_shopping_bag(String productSize) throws Throwable {
+		assertTrue(productSize + " color should have been displayed",
+				shoppingBagPage.isProductSizeDisplayed(productSize));
+	}
+
+	@And("^Verify ([^\"]*) items are specified as quantity in shopping bag$")
+	public void verify_items_are_specified_as_quantity(String productQuantity) throws Throwable {
+		assertEquals("Product Quantity should be the same", productQuantity,
+				shoppingBagPage.getItemQuantity());
+	}
+
+	@And("^Verify product name is the same as the one displayed in pdp page$")
+	public void verify_product_name_is_the_same_as_the_one_displayed_in_pdp_page() throws Throwable {
+		String productName = stateHolder.get("productName").toUpperCase();
+		assertTrue(productName + " product is not the one displayed",
+				shoppingBagPage.getProductName().endsWith(productName));
+	}
+
+	@And("^Verify color is the one selected from pdp page$")
+	public void verify_color_is_the_one_selected_from_pdp_page() throws Throwable {
+		String expectedColor = stateHolder.get("color");
+		assertTrue(expectedColor + " color is not displayed", shoppingBagPage.isProductColorDisplayed(expectedColor));
+	}
+
+	@And("^Verify size is the one selected from pdp page$")
+	public void verify_size_is_the_one_selected_from_pdp_page() throws Throwable {
+		String expectedSize = stateHolder.get("size");
+		assertTrue(expectedSize + " size is not displayed", shoppingBagPage.isProductSizeDisplayed(expectedSize));
+	}
+
+	@And("^Verify price is the same as the one displayed in pdp page$")
+	public void verify_price_is_the_same_as_the_one_displayed_in_pdp_page() throws Throwable {
+		String price = stateHolder.get("priceList") == null ? stateHolder.get("priceWas") : stateHolder.get("priceList");
+		assertEquals("Price should be the same", price, shoppingBagPage.getSubtotalValue());
+	}
+
+	@And("^Verify variation is the one selected from pdp page$")
+	public void verify_variation_is_the_one_selected_from_pdp_page() throws Throwable {
+		String variation = stateHolder.get("variation");
+		if (variation != null) {
+			String prefix = variation.toUpperCase();
+			if (!"REGULAR".equalsIgnoreCase(prefix)) {
+				assertTrue(prefix + " variation should have been displayed as part of the product name:",
+						shoppingBagPage.getProductName().startsWith(prefix));
+			}
+		}
+	}
+
+
+	@Then("change URL to mobile URL")
+	public void change_URL_To_Mobile_URL(){
+
+		//Get the current URL and append the ?isMobile=true
+		String currentURL = getDriver().getCurrentUrl().trim();
+		String browserURL = currentURL.replaceAll("sidecar=true", "");
+		browserURL = browserURL + "isMobile=true";
+
+		//navigate to the mobile URL
+		getDriver().navigate().to(browserURL);
+		getDriver().manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+		reporting.takeScreenshot(scenario);
+	}
+
+	@Then("verify mobile page is displayed")
+	public void verify_mobile_page_displayed(){
+		shoppingBagPage.verifyMobilePageDisplayed();
+
+		reporting.takeScreenshot(scenario);
+	}
 }
