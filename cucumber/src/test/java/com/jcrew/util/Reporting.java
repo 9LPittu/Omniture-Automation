53de8e7e7aeb86314118540c416a24fc0b88feca
Hima Venaganti <Hima.Venaganti.cft@jcrew.com>
2015-10-05 10:59:46 -0400

    Fixing code so it does not break when the screenshot property does not exist.

--- cucumber/src/test/java/com/jcrew/util/Reporting.java
+++ cucumber/src/test/java/com/jcrew/util/Reporting.java
@@ -6,21 +6,19 @@ import org.openqa.selenium.WebDriver;
 
 import cucumber.api.Scenario;
 
-public class Reporting {  
-    
-    public void takeScreenshot(Scenario scenario){ 	
-		
-	      try {       	
-	      	final PropertyReader propertyReader = PropertyReader.getPropertyReader();        	
-	          WebDriver driver = new DriverFactory().getDriver();
-	      	if(propertyReader.getScreenshotForEveryStep().equalsIgnoreCase("yes")){
-	      		byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);        		
-	      		scenario.embed(screenshot, "image/png");
-	      	}
-	      }
-	    
-	      catch (Exception e) {
-	          e.printStackTrace();  
-	      }     
+public class Reporting {
+
+    public void takeScreenshot(Scenario scenario) {
+
+        try {
+            final PropertyReader propertyReader = PropertyReader.getPropertyReader();
+            WebDriver driver = new DriverFactory().getDriver();
+            if ("yes".equalsIgnoreCase(propertyReader.getScreenshotForEveryStep())) {
+                byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot, "image/png");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
