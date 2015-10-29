b047724c3a068999233f7714cf1a3ef8b4a0db52
Ulises Pulido <ulises.pulido.cft@jcrew.com>
2015-10-29 11:41:22 -0400

    Adding time out description message

--- cucumber/src/main/java/com/jcrew/steps/ShippingAddressPageSteps.java
+++ cucumber/src/main/java/com/jcrew/steps/ShippingAddressPageSteps.java
@@ -4,9 +4,9 @@ import com.jcrew.page.ShippingAddressPage;
 import com.jcrew.util.DriverFactory;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.TimeoutException;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class ShippingAddressPageSteps extends DriverFactory {
 
@@ -16,8 +16,14 @@ public class ShippingAddressPageSteps extends DriverFactory {
     public void fills_shipping_address() throws Throwable {
 
         shippingAddressPage.fills_shipping_address();
+        try {
 
-        assertEquals("New York, NY", shippingAddressPage.getSelectedCityAndState());
+            assertEquals("New York, NY", shippingAddressPage.getSelectedCityAndState());
+
+        } catch (TimeoutException te) {
+            fail("Unable to retrieve Dropdown menu containing City/State for the corresponding zip code 10003." +
+                    " Service unavailable");
+        }
 
         assertTrue("Billing checkbox should be selected", shippingAddressPage.isBillingAndShippingSameAddress());
 
