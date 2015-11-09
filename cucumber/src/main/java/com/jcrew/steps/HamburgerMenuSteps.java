4f12562885046593a07f3a063a174e741ec47165
Javier Cervantes <1.27201@gmail.com>
2015-11-09 11:21:58 -0500

    Spike on how to arrange features without UI details

--- cucumber/src/main/java/com/jcrew/steps/HamburgerMenuSteps.java
+++ cucumber/src/main/java/com/jcrew/steps/HamburgerMenuSteps.java
@@ -17,33 +17,33 @@ public class HamburgerMenuSteps extends DriverFactory {
     private final StateHolder stateHolder = StateHolder.getInstance();
 
     @Then("^Hamburger Menu ([^\"]*) Link is present$")
-    public void hamburger_menu_category_link_is_present(String category) throws Throwable {
+    public void hamburger_menu_category_link_is_present(String category) {
         assertTrue(category + " category should have been present", hamburgerMenu.isCategoryPresent(category));
     }
 
     @When("^User clicks on ([^\"]*) subcategory from ([^\"]*) Category$")
-    public void user_clicks_on_subcategory_from_category(String subcategory, String category) throws Throwable {
+    public void user_clicks_on_subcategory_from_category(String subcategory, String category) {
         hamburgerMenu.click_on_subcategory(subcategory, category);
     }
 
     @Then("^Closes subcategory hamburger menu$")
-    public void closes_subcategory_hamburger_menu() throws Throwable {
+    public void closes_subcategory_hamburger_menu() {
         hamburgerMenu.close_subcategory_hamburger_menu();
     }
 
 
     @Then("^Hamburger menu is present$")
-    public void hamburger_menu_is_present() throws Throwable {
+    public void hamburger_menu_is_present() {
         assertTrue("Hamburger menu is present", hamburgerMenu.isHamburgerMenuPresent());
     }
 
     @Given("^User clicks on hamburger menu$")
-    public void user_clicks_on_hamburger_menu() throws Throwable {
+    public void user_clicks_on_hamburger_menu() {
         hamburgerMenu.click_on_hamburger_menu();
     }
 
     @And("^Selects ([^\"]*) Category from hamburger menu$")
-    public void selects_category_from_hamburger_menu(String category) throws Throwable {
+    public void selects_category_from_hamburger_menu(String category) {
         hamburgerMenu.click_on_category(category);
         stateHolder.put("category", category);
     }
@@ -54,28 +54,37 @@ public class HamburgerMenuSteps extends DriverFactory {
     }
 
     @And("^Goes to sign in page$")
-    public void goes_to_sign_in_page() throws Throwable {
+    public void goes_to_sign_in_page() {
         hamburgerMenu.click_on_sign_in_link();
     }
 
     @And("^Closes hamburger menu$")
-    public void closes_hamburger_menu() throws Throwable {
+    public void closes_hamburger_menu() {
         hamburgerMenu.close_hamburger_menu();
     }
 
     @And("^Chooses a random category$")
-    public void chooses_a_random_category() throws Throwable {
+    public void chooses_a_random_category() {
         hamburgerMenu.click_random_category();
     }
 
     @And("^Chooses a random subcategory$")
-    public void chooses_a_random_subcategory() throws Throwable {
+    public void chooses_a_random_subcategory() {
         hamburgerMenu.click_random_subcategory();
     }
 
     @And("^User is signed out$")
-    public void user_is_signed_out() throws Throwable {
+    public void user_is_signed_out() {
         assertEquals("Expected sign in message is not there",
                 "SIGN IN", hamburgerMenu.getSignInMessage());
     }
+
+//    @When("^User selects ([^\"]*) for ([^\"]*)")
+//    public void select_subcategory(String subcategory, String category) {
+//        hamburgerMenu.click_on_hamburger_menu();
+//        hamburgerMenu.click_on_category(category);
+//        stateHolder.put("category", category);
+//        hamburgerMenu.click_on_subcategory(subcategory, category);
+//    }
+
 }
