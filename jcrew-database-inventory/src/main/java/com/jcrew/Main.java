5f221145d140180ea4d4e0718001906ea390a205
Hima Venaganti <Hima.Venaganti.cft@jcrew.com>
2015-11-18 16:50:24 -0500

    deleting TestDataReader and testdata property file

--- jcrew-database-inventory/src/main/java/com/jcrew/Main.java
+++ jcrew-database-inventory/src/main/java/com/jcrew/Main.java
@@ -12,31 +12,9 @@ public class Main {
 
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         InventoryDAO inventoryDAO = new InventoryDAO();
-
-        String[] steeltestdata = new String[15];
         final Properties propertyReader = new Properties();
-        propertyReader.load(new FileReader("steelQAtestdata.properties"));
-        steeltestdata[0] = propertyReader.getProperty("backorderitem");
-        steeltestdata[1] = propertyReader.getProperty("particularcolorsizesoldoutitem");
-        steeltestdata[2] = propertyReader.getProperty("soldoutitem");
-        steeltestdata[3] = propertyReader.getProperty("notavailabletoshipitem");
-        steeltestdata[4] = propertyReader.getProperty("preorderitem");
-        steeltestdata[5] = propertyReader.getProperty("VPSoutofstockitem");
-        steeltestdata[6] = propertyReader.getProperty("finalsaleitem");
-        steeltestdata[7] = propertyReader.getProperty("itemwithonlyonevariation");
-        steeltestdata[8] = propertyReader.getProperty("itemwithonlyonesalesku");
-        steeltestdata[9] = propertyReader.getProperty("itemwithmorethanonesku");
-        steeltestdata[10] = propertyReader.getProperty("itemwithmultiplecolorsfullsku");
-        steeltestdata[11] = propertyReader.getProperty("itemwithmultiplecolorssalesku");
-        String[] bronzetestdata = {};
+        propertyReader.load(new FileReader(args[0] + "QAtestdata.properties"));
 
-        if(args[0].equals("steel"))
-                            inventoryDAO.addInventory(steeltestdata);
-        else {
-            if(args[0].equals("bronze"))
-                inventoryDAO.addInventory(bronzetestdata);
-
-        }
-
+        inventoryDAO.addInventory(propertyReader);
     }
 }
--- jcrew-database-inventory/src/main/java/com/jcrew/util/InventoryDAO.java
+++ jcrew-database-inventory/src/main/java/com/jcrew/util/InventoryDAO.java
@@ -8,95 +8,29 @@ import java.util.Properties;
 public class InventoryDAO {
 
     public static final String UPDATE_BACKORDER_ITEM = "Update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 0, sellable_oh_rtl = 0, sellable_br_qty = 50, sellable_oo_qty = 10 , BR_FLAG = 'Y' where variant = 'C8972GY66892'";
-    public static final String UPDATE_PARTICULAR_COLOR_SIZE_SOLDOUT_ITEM = "Update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 1, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant = 'E3029ST14778'";
-    public static final String UPDATE_SOLDOUT_ITEM = "Update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 0, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like 'E4308%'";
-    public static final String UPDATE_NOT_AVAILABLE_TO_SHIP_ITEM = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 0, sellable_oh_rtl = 0 , sellable_br_qty = 0, sellable_oo_qty = 20, br_flag = 'Y', po_number = '123456', po_date = '15-Dec-15' where variant = 'B0517WN29172'";
-    public static final String UPDATE_PRE_ORDER_ITEM = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 0, sellable_oh_rtl = 0 , sellable_br_qty = 0, sellable_oo_qty  = 10, br_flag = 'Y', po_number = '123456', po_date = '15-Dec-15' where variant like 'C9220%'";
-    public static final String UPDATE_OUT_OF_STOCK_VPS_ITEM = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 0, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like '18040%'";
-    public static final String UPDATE_FINAL_SALE_ITEM = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 20, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like 'B9447%'";
-    public static final String UPDATE_ITEM_WITH_ONLY_ONE_VARIATION = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 50, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like 'B8752%'";
-    public static final String UPDATE_ITEM_WITH_ONLY_ONE_SALE_SKU = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 20, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like 'B7701%'";
-    public static final String UPDATE_ITEM_WITH_MORE_THAN_ONE_SKU = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 20, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like 'E3835%'";
-    public static final String UPDATE_ITEM_WITH_MULTIPLE_COLORS_FULL_SKU = "update JCBRNQA_STORE.jc_web_inventory set sellable_oh_qty = 50, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like 'C3409%'";
-    public static final String UPDATE_ITEM_WITH_MULTIPLE_COLORS_SALE_SKU = "update JCBRNQA_STORE.jc_web_inventory  set sellable_oh_qty = 50, sellable_oh_rtl = 0, sellable_br_qty = 0, sellable_oo_qty = 0 where variant like 'E3896%'";
-
-
-    public ResultSet GetData(String strQuery) {
-
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-
-            final Properties propertyReader = new Properties();
-            String url = "jdbc:oracle:thin:@" + propertyReader.getProperty("db.server.name") + ":1521/" + propertyReader.getProperty("db.server.servicename");
-            Properties props = new Properties();
-            Connection con = createConnection(url, props);
-            Statement stmt = con.createStatement();
-
-            props.setProperty("user", propertyReader.getProperty("db.server.user"));
-            props.setProperty("password", propertyReader.getProperty("db.server.pwd"));
-            props.setProperty("ssl", "true");
-
-            return stmt.executeQuery(strQuery);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return null;
-    }
 
     public int addInventoryToBackOrderedItem() throws SQLException, ClassNotFoundException, IOException {
-        String strQuery = UPDATE_BACKORDER_ITEM;
         Connection conn = getConnectionToDatabase();
         Statement statement = createTheStatement(conn);
-        int rowsUpdated = executeQueryToAddInventory(statement, strQuery);
-         closeConnection(conn);
+        int rowsUpdated = executeQueryToAddInventory(statement, UPDATE_BACKORDER_ITEM);
+        closeConnection(conn);
         return rowsUpdated;
     }
 
-    public void addInventory() throws SQLException, ClassNotFoundException, IOException {
-        String strBackorderQuery = UPDATE_BACKORDER_ITEM;
-        String strparticularSoldOutQuery = UPDATE_PARTICULAR_COLOR_SIZE_SOLDOUT_ITEM;
-        String strSoldOutQuery = UPDATE_SOLDOUT_ITEM;
-        String strShipWarningMessageQuery = UPDATE_NOT_AVAILABLE_TO_SHIP_ITEM;
-        String strPreOrderQuery = UPDATE_PRE_ORDER_ITEM;
-        String strVPSOutOfStockQuery = UPDATE_OUT_OF_STOCK_VPS_ITEM;
-        String strFinalSaleQuery = UPDATE_FINAL_SALE_ITEM;
-        String strItemWithOnlyOneVariationQuery = UPDATE_ITEM_WITH_ONLY_ONE_VARIATION;
-        String strItemWithOnlyOneSaleSkuQuery = UPDATE_ITEM_WITH_ONLY_ONE_SALE_SKU;
-        String strItemWithMoreThanOneSku = UPDATE_ITEM_WITH_MORE_THAN_ONE_SKU;
-        String strItemWithMultipleColorsFullSku = UPDATE_ITEM_WITH_MULTIPLE_COLORS_FULL_SKU;
-        String strItemWithMultipleColorsSaleSku = UPDATE_ITEM_WITH_MULTIPLE_COLORS_SALE_SKU;
-        Connection conn = getConnectionToDatabase();
-        Statement statement = createTheStatement(conn);
+    public void addInventory(Properties propertyReader) throws SQLException, ClassNotFoundException, IOException {
 
-        System.out.println("back order item: "+executeQueryToAddInventory(statement, strBackorderQuery));
-        System.out.println("particular Sold out item: "+executeQueryToAddInventory(statement, strparticularSoldOutQuery));
-        System.out.println("Sold out : "+executeQueryToAddInventory(statement, strSoldOutQuery));
-        System.out.println("Ship warning: "+executeQueryToAddInventory(statement, strShipWarningMessageQuery));
-        System.out.println("Pre order: "+executeQueryToAddInventory(statement, strPreOrderQuery));
-        System.out.println("VPS Out of Stock: "+executeQueryToAddInventory(statement, strVPSOutOfStockQuery));
-        System.out.println("Final Sale : "+executeQueryToAddInventory(statement, strFinalSaleQuery));
-        System.out.println("Item with only one variation: "+executeQueryToAddInventory(statement,strItemWithOnlyOneVariationQuery));
-        System.out.println("One sale sku: "+executeQueryToAddInventory(statement,strItemWithOnlyOneSaleSkuQuery));
-        System.out.println("Item with more than one sku: "+executeQueryToAddInventory(statement,strItemWithMoreThanOneSku));
-        System.out.println("Item with multiple colors full sku: "+executeQueryToAddInventory(statement,strItemWithMultipleColorsFullSku));
-        System.out.println("Item with multiple colors Sale sku: "+executeQueryToAddInventory(statement,strItemWithMultipleColorsSaleSku));
-
-        closeConnection(conn);
-    }
-
-    public void addInventory(String[] testdata) throws SQLException, ClassNotFoundException, IOException {
-        String strBackOrderQuery = updateBackOrderItem(testdata[0]);
-        String strparticularSoldOutQuery = updateParticularColorSizeSoldOutItem(testdata[1]);
-        String strSoldOutQuery = updateSoldOutItem(testdata[2]);
-        String strShipWarningMessageQuery = updateNotAvailableToShip(testdata[3]);
-        String strPreOrderQuery = updatePreOrderItem(testdata[4]);
-        String strVPSOutOfStockQuery = updateVPSOutOfStockItem(testdata[5]);
-        String strFinalSaleQuery = updateFinalSaleItem(testdata[6]);
-        String strItemWithOnlyOneVariationQuery = updateItemWithOnlyOneVariation(testdata[7]);
-        String strItemWithOnlyOneSaleSkuQuery = updateItemWithOnlyOneSaleSku(testdata[8]);
-        String strItemWithMoreThanOneSku = updateItemWithMoreThanOneSku(testdata[9]);
-        String strItemWithMultipleColorsFullSku = updateItemWithMultipleColorsFullSku(testdata[10]);
-        String strItemWithMultipleColorsSaleSku = updateItemWithMultipleColorsSaleSku(testdata[11]);
+        String strBackOrderQuery = updateBackOrderItem(propertyReader.getProperty("backorderitem"));
+        String strparticularSoldOutQuery = updateParticularColorSizeSoldOutItem(propertyReader.getProperty("particularcolorsizesoldoutitem"));
+        String strSoldOutQuery = updateSoldOutItem(propertyReader.getProperty("soldoutitem"));
+        String strShipWarningMessageQuery = updateNotAvailableToShip(propertyReader.getProperty("notavailabletoshipitem"));
+        String strPreOrderQuery = updatePreOrderItem(propertyReader.getProperty("preorderitem"));
+        String strVPSOutOfStockQuery = updateVPSOutOfStockItem(propertyReader.getProperty("VPSoutofstockitem"));
+        String strFinalSaleQuery = updateFinalSaleItem(propertyReader.getProperty("finalsaleitem"));
+        String strItemWithOnlyOneVariationQuery = updateItemWithOnlyOneVariation(propertyReader.getProperty("itemwithonlyonevariation"));
+        String strItemWithOnlyOneSaleSkuQuery = updateItemWithOnlyOneSaleSku(propertyReader.getProperty("itemwithonlyonesalesku"));
+        String strItemWithMoreThanOneSku = updateItemWithMoreThanOneSku(propertyReader.getProperty("itemwithmorethanonesku"));
+        String strItemWithMultipleColorsFullSku = updateItemWithMultipleColorsFullSku(propertyReader.getProperty("itemwithmultiplecolorsfullsku"));
+        String strItemWithMultipleColorsSaleSku = updateItemWithMultipleColorsSaleSku(propertyReader.getProperty("itemwithmultiplecolorssalesku"));
         Connection conn = getConnectionToDatabase();
         Statement statement = createTheStatement(conn);
 
@@ -177,13 +111,6 @@ public class InventoryDAO {
         return itemWithMultipleColorsSaleSkuQuery;
     }
 
-
-
-
-
-
-
-
     public Connection getConnectionToDatabase() throws ClassNotFoundException, SQLException, IOException {
             Class.forName("oracle.jdbc.driver.OracleDriver");
 
@@ -229,5 +156,4 @@ public class InventoryDAO {
             System.out.println(e);
         }
     }
-
 }
