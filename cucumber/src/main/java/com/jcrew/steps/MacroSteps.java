90d7d132badc2b9e397e4e11577dd6de365e05fe
qatester1 <vbonker@csc.com>
2015-12-10 09:18:55 -0500

    Added new scenarios for Embedded footer

--- cucumber/src/main/java/com/jcrew/steps/MacroSteps.java
+++ cucumber/src/main/java/com/jcrew/steps/MacroSteps.java
@@ -5,6 +5,7 @@ import cucumber.api.java.en.Then;
 public class MacroSteps {
 
     private final HeaderSteps headerSteps = new HeaderSteps();
+    private final FooterSteps footerSteps = new FooterSteps();
     private final HamburgerMenuSteps hamburgerMenuSteps = new HamburgerMenuSteps();
     private final ShoppingBagSteps shoppingBagSteps = new ShoppingBagSteps();
 
@@ -24,4 +25,34 @@ public class MacroSteps {
         headerSteps.user_clicks_on_item_bag();
         shoppingBagSteps.user_should_be_in_shopping_bag_page();
     }
+    
+    @Then("^Verify embedded footer is visible and functional$")
+    public void verify_embedded_footer_visible_and_functional() throws Throwable{
+    	
+    	footerSteps.click_con_header_from_footer("Contact Us");
+    	
+    	footerSteps.verify_icon_and_text_is_displayed("twitter");
+    	footerSteps.verify_icon_and_text_is_displayed("phone");
+    	footerSteps.verify_icon_and_text_is_displayed("vps");
+    	
+    	footerSteps.verify_footer_link_is_displayed("Let Us Help You");
+    	footerSteps.verify_footer_link_is_displayed("Our Cards");
+    	footerSteps.verify_footer_link_is_displayed("Our Stores");    	
+    	footerSteps.verify_footer_link_is_displayed("Our Brands");
+    	footerSteps.verify_footer_link_is_displayed("About J.Crew");
+    	
+    	footerSteps.verify_footer_header_is_displayed("LIKE BEING FIRST?");
+    	footerSteps.verify_email_field_is_displayed();
+    	
+    	footerSteps.verify_footer_link_is_displayed("Get To Know Us");
+    	
+    	footerSteps.verify_social_icons_are_displayed("facebook");
+    	footerSteps.verify_social_icons_are_displayed("twitter");
+    	footerSteps.verify_social_icons_are_displayed("tumblr");
+    	footerSteps.verify_social_icons_are_displayed("pinterest");
+    	footerSteps.verify_social_icons_are_displayed("instagram");
+    	footerSteps.verify_social_icons_are_displayed("google");
+    	footerSteps.verify_social_icons_are_displayed("youtube");    	
+    }
+    
 }
