bab7d2641c6d65b04dd072f5f73797c978e53a6e
Javier Cervantes <javier@sawyereffect.com>
2015-12-11 16:38:33 -0500

    Adding retry 2 times for intial page

--- cucumber/src/main/java/com/jcrew/steps/StartingSteps.java
+++ cucumber/src/main/java/com/jcrew/steps/StartingSteps.java
@@ -11,17 +11,13 @@ import cucumber.api.java.Before;
 import cucumber.api.java.BeforeStep;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
-import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 
 public class StartingSteps {
 
@@ -40,10 +36,20 @@ public class StartingSteps {
 
     @Given("^User is on homepage$")
     public void user_is_on_home_page() {
-        driver.manage().timeouts().implicitlyWait(Util.DEFAULT_TIMEOUT, TimeUnit.SECONDS);
-        getIntialPage();
-        waitForPageToLoadUpToTheLastElementPriorScriptExecution();
-        Util.waitForPageFullyLoaded(driver);
+        int retry = 0;
+        boolean successfulLoad = false;
+        while (retry < 2 && !successfulLoad) {
+            try {
+                driver.manage().timeouts().implicitlyWait(Util.DEFAULT_TIMEOUT, TimeUnit.SECONDS);
+                getIntialPage();
+                waitForPageToLoadUpToTheLastElementPriorScriptExecution();
+                Util.waitForPageFullyLoaded(driver);
+                successfulLoad = true;
+            } catch (TimeoutException te) {
+                logger.debug("Page did not load retry: {}", retry + 1);
+                retry++;
+            }
+        }
     }
 
     private void waitForPageToLoadUpToTheLastElementPriorScriptExecution() {
