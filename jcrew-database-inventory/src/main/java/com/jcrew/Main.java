2a43b0590feb9ef23072eadcd7dca4e4c603082f
Hima Venaganti <Hima.Venaganti.cft@jcrew.com>
2015-12-16 23:24:54 -0500

    test data additions to their corresponding environments

--- jcrew-database-inventory/src/main/java/com/jcrew/Main.java
+++ jcrew-database-inventory/src/main/java/com/jcrew/Main.java
@@ -13,7 +13,9 @@ public class Main {
     public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
         InventoryDAO inventoryDAO = new InventoryDAO();
         final Properties propertyReader = new Properties();
+        final Properties databaseReader = new Properties();
         propertyReader.load(new FileReader(args[0] + "QAtestdata.properties"));
+        propertyReader.load(new FileReader(args[0] + "database.properties"));
 
         inventoryDAO.addInventory(propertyReader);
     }
