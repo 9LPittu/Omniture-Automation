314a0ea531c5f1757815aa2ab4c14d793a54f03f
Hima Venaganti <Hima.Venaganti.cft@jcrew.com>
2016-01-08 10:11:41 -0500

    more modifications to footer

--- cucumber/src/main/java/com/jcrew/page/Footer.java
+++ cucumber/src/main/java/com/jcrew/page/Footer.java
@@ -225,7 +225,8 @@ public class Footer {
     
     public void clickViewFullSite(){
 
-        WebElement viewFullSiteLink = driver.findElement(By.xpath("//*[@id=\"global__footer\"]/div/div[3]/div/div/a"));
+       // WebElement viewFullSiteLink = driver.findElement(By.xpath("//*[@id=\"global__footer\"]/div/div[3]/div/div/a"));
+        WebElement viewFullSiteLink = driver.findElement(By.xpath("//a[text()='View full site']"));
         Util.createWebDriverWait(driver).until(ExpectedConditions.elementToBeClickable(viewFullSiteLink));
     	viewFullSiteLink.click();
     }
