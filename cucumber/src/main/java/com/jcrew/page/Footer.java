2bc0e17d83875bc8b1a15389cff2fcdabe9ca808
Nadia Paola Garcia <nadia@sawyereffect.com>
2016-02-02 16:41:45 -0500

    - Added wait to verify that browser has changed url - Fix for shipping methods

--- cucumber/src/main/java/com/jcrew/page/Footer.java
+++ cucumber/src/main/java/com/jcrew/page/Footer.java
@@ -219,9 +219,11 @@ public class Footer {
     }
     
     public void clickSocialSharingIcon(String socialSharingIconName){
+        String currentURL = driver.getCurrentUrl();
     	WebElement socialSharingIcon = driver.findElement(By.xpath("//ul[@class='footer__social__menu']/" +
                 "descendant::li/a/i[contains(@class,'icon-social-" + socialSharingIconName.toLowerCase() + "')]"));
     	socialSharingIcon.click();
+        Util.createWebDriverWait(driver).until(ExpectedConditions.not(ExpectedConditions.urlToBe(currentURL)));
     }
     
     public boolean isViewFullSiteDisplayedAfterLegalLinks(){
