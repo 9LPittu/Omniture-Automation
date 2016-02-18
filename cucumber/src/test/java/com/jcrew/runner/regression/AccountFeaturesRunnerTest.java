22639ab2be97417bfa46e5c0957a2e3a89c16a0a
Hima Venaganti <Hima.Venaganti.cft@jcrew.com>
2016-02-18 12:06:04 -0500

    temporary change...to run only one scenario

--- cucumber/src/test/java/com/jcrew/runner/regression/AccountFeaturesRunnerTest.java
+++ cucumber/src/test/java/com/jcrew/runner/regression/AccountFeaturesRunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"features/regression"},
-        tags = {"@Account"},
+        tags = {"@new"},
         glue = {"com.jcrew.steps"},
         format = {
                 "json:target/cucumber-accountregressionfeatures.json",
