cf75b71f882091963883942f2e7d6cf98cf50a99
Hima Venaganti <Hima.Venaganti.cft@jcrew.com>
2016-02-18 12:19:04 -0500

    changing it back to original

--- cucumber/src/test/java/com/jcrew/runner/regression/AccountFeaturesRunnerTest.java
+++ cucumber/src/test/java/com/jcrew/runner/regression/AccountFeaturesRunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = {"features/regression"},
-        tags = {"@new"},
+        tags = {"@Account"},
         glue = {"com.jcrew.steps"},
         format = {
                 "json:target/cucumber-accountregressionfeatures.json",
