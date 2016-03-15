7befe2783be85749241c5b2170571d35394c941e
Nadia Paola Garcia <nadia@sawyereffect.com>
2016-03-15 17:22:02 -0400

    - synch fixes

--- cucumber/src/main/java/com/jcrew/page/MyAccountPage.java
+++ cucumber/src/main/java/com/jcrew/page/MyAccountPage.java
@@ -96,7 +96,7 @@ public class MyAccountPage {
             WebElement deleteButton = tables.get(1).findElement(By.linkText("DELETE"));
             deleteButton.click();
 
-            Util.waitLoadingBar(driver);
+            Util.waitForPageFullyLoaded(driver);
             tables = driver.findElements(By.xpath("//div[@id='creditCardList']/table"));
         }
     }
