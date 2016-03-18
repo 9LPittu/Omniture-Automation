b46867c0c28e8c53bd77fb034bcd203783f1cfbf
Nadia Paola Garcia <nadia@sawyereffect.com>
2016-03-18 09:34:07 -0400

    Fix removing non-default addresses and non-default credit cards

--- cucumber/src/main/java/com/jcrew/page/MyAccountPage.java
+++ cucumber/src/main/java/com/jcrew/page/MyAccountPage.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.jcrew.util.PropertyReader;
 import com.jcrew.util.Util;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
