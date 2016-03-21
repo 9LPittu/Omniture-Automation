456394e15c8f193a562ba6eaed170a1392d91592
swapna.reddivari <swapna.reddivari.cft@jcrew.com>
2016-03-21 09:02:06 -0400

    Uncommented implicit wait on phantomjs

--- cucumber/src/main/java/com/jcrew/util/DriverFactory.java
+++ cucumber/src/main/java/com/jcrew/util/DriverFactory.java
@@ -204,7 +204,7 @@ public class DriverFactory {
             capabilities.setCapability("phantomjs.page.settings.userAgent", propertyReader.getProperty("user.agent"));
 
             driver = getDesktopWebDriver(propertyReader, capabilities);
-            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            //driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         }
 
         //driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
