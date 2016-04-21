08fcb10eb88a49caa01c91c360b605b771597fcc
Hima Venaganti <Hima.Venaganti.cft@Jcrew.com>
2016-04-21 16:49:07 -0400

    Adding runner for the international context feature

--- cucumber/features/regression/InternationalContextCurrency.feature
+++ cucumber/features/regression/InternationalContextCurrency.feature
@@ -1,4 +1,4 @@
-@HeaderAndFooter
+@Context
 Feature:
 
   Background:
---
+++ cucumber/src/test/java/com/jcrew/runner/regression/InternationalContextRunnerTest.java
@@ -0,0 +1,25 @@
+package com.jcrew.runner.regression;
+
+/**
+ * Created by 9hvenaga on 4/21/2016.
+ */
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"features/regression"},
+        tags = {"@Context"},
+        glue = {"com.jcrew.steps"},
+        format = {
+                "json:target/cucumber-internationalcontextregressionfeatures.json",
+                "pretty",
+                "html:target/cucumber/internationalcontextregressionfeatures"
+        }
+)
+
+
+
+public class InternationalContextRunnerTest {
+}
