e53d8c7347bc760d58b8adbd7b45e82c374f8480
Hima Venaganti <Hima.Venaganti.cft@Jcrew.com>
2016-05-10 15:10:21 -0400

    changing the keywords

--- desktop-functional-tests/src/main/java/com/jcrew/steps/ForgotPasswordSteps.java
+++ desktop-functional-tests/src/main/java/com/jcrew/steps/ForgotPasswordSteps.java
@@ -3,13 +3,14 @@ package com.jcrew.steps;
 import com.jcrew.page.ForgotPassword;
 import com.jcrew.utils.DriverFactory;
 import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 
 import static org.junit.Assert.assertTrue;
 
 public class ForgotPasswordSteps extends DriverFactory {
     private final ForgotPassword forgotPasswordPage = new ForgotPassword(getDriver());
 
-    @And("^Verify user is in forgot password page$")
+    @Then("^Verify user is in forgot password page$")
     public void verify_user_is_in_forgot_password_page() throws Throwable {
         assertTrue("User should have been in forgot password page", forgotPasswordPage.isForgotPasswordPage());
     }
