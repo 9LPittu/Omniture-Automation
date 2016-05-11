cff662f4532b20ad4c8ea67aa14efef67e497d8f
Hima Venaganti <Hima.Venaganti.cft@Jcrew.com>
2016-05-11 00:15:09 -0400

    formatting

--- desktop-functional-tests/src/main/java/com/jcrew/steps/SearchArraySteps.java
+++ desktop-functional-tests/src/main/java/com/jcrew/steps/SearchArraySteps.java
@@ -1,32 +1,32 @@
 package com.jcrew.steps;
 
 import com.jcrew.page.SearchArray;
-import com.jcrew.utils.DriverFactory;
+        import com.jcrew.utils.DriverFactory;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+        import cucumber.api.java.en.Given;
+        import cucumber.api.java.en.Then;
+        import cucumber.api.java.en.When;
 
-import static org.junit.Assert.*;
+        import static org.junit.Assert.*;
 
 /**
  * Created by nadiapaolagarcia on 4/1/16.
  */
 public class SearchArraySteps extends DriverFactory {
-    
-	SearchArray productsArray = new SearchArray(getDriver());
+
+    SearchArray productsArray = new SearchArray(getDriver());
 
 
     @When("User selects random product from search array")
-    public void user_selects_random_product(){
+    public void user_selects_random_product() {
         productsArray.selectRandomProduct();
     }
-    
+
     @Then("User is in search results page")
     public void user_is_in_search_results_page() {
 
         assertTrue("User should be in search page", productsArray.isSearchPage());
-        
+
 
     }
 
