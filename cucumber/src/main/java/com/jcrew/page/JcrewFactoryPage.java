a1ae9584c047300fcdb88a451ce02b09f716d574
Vithal <vbonker@csc.com>
2016-05-25 11:58:36 -0400

    added print statement

--- cucumber/src/main/java/com/jcrew/page/JcrewFactoryPage.java
+++ cucumber/src/main/java/com/jcrew/page/JcrewFactoryPage.java
@@ -1,6 +1,9 @@
 package com.jcrew.page;
 
 import com.jcrew.util.Util;
+
+import ch.qos.logback.classic.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -18,6 +21,7 @@ public class JcrewFactoryPage {
 
     public Boolean isJcrewFactoryPage() {
         ArrayList<String> tabs = new ArrayList<String> (driver.getWindowHandles());
+        System.out.println("# of tabs opened:" + tabs.size());
         driver.switchTo().window(tabs.get(1));
         return Util.createWebDriverWait(driver).until(ExpectedConditions.urlContains("factory.jcrew.com"));
     }
