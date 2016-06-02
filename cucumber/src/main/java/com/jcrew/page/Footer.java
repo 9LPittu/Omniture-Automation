f5653d778d00957ccf29403a91034031b3230c65
Hima Venaganti <Hima.Venaganti.cft@Jcrew.com>
2016-06-02 13:02:28 -0400

    adding wait statement for the footer section to be visible before locating the country name in the footer

--- cucumber/src/main/java/com/jcrew/page/Footer.java
+++ cucumber/src/main/java/com/jcrew/page/Footer.java
@@ -481,6 +481,7 @@ public class Footer {
     }
 
     public WebElement getCountryNameFooterElement() {
+        Util.createWebDriverWait(driver).until(ExpectedConditions.visibilityOf(footerSection));
         return footerSection.findElement(By.xpath(".//div[@class='footer__country-context']/descendant::span[@class='footer__country-context__country']"));
     }
 }
