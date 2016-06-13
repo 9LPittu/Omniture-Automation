f953cc16e0428c3818fc45ffbe19fa9f52a4560c
Hima Venaganti <Hima.Venaganti.cft@Jcrew.com>
2016-06-13 12:56:48 -0400

    modification

--- desktop-functional-tests/src/main/java/com/jcrew/page/LogIn.java
+++ desktop-functional-tests/src/main/java/com/jcrew/page/LogIn.java
@@ -105,7 +105,8 @@ public class LogIn extends DriverFactory {
         createAccountFormIsDisplayed();
         createAnAccount = registerForm.findElement(By.tagName("button"));
         createAnAccount.click();
-        wait.until(ExpectedConditions.not(ExpectedConditions.urlContains("/r/login")));
+       // wait.until(ExpectedConditions.not(ExpectedConditions.urlContains("/r/login")));
+        Util.waitForPageFullyLoaded(driver);
     }
 
     private WebElement getNewUserField(String field) {
