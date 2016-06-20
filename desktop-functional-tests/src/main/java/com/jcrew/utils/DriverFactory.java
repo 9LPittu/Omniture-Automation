4d6559471c254f445edd2b213feac68cefc31df5
Nadia Paola Garcia <nadia@sawyereffect.com>
2016-06-20 13:46:14 -0400

    Logging DEBUG messages in browser

--- desktop-functional-tests/src/main/java/com/jcrew/utils/DriverFactory.java
+++ desktop-functional-tests/src/main/java/com/jcrew/utils/DriverFactory.java
@@ -33,7 +33,8 @@ public class DriverFactory {
             "--local-to-remote-url-access=true",
             "--disk-cache=false",
             "--ignore-ssl-errors=true",
-            "--webdriver-loglevel=ERROR"
+            "--webdriver-loglevel=DEBUG",
+            "--remote-debugger-port=9000"
     };
     private static final Map<String, WebDriver> driverMap = new HashMap<>();
     private final int DEFAULT_WINDOW_WIDTH = 400;
