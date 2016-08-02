6f4946eda0ef3d9968f8094638044afc2e8d5ba6
Swapna Reddivari <swapna.reddivari.cft@jcrew.com>
2016-08-02 13:31:04 -0400

    Increased timeout

--- cucumber/src/main/java/com/jcrew/util/Util.java
+++ cucumber/src/main/java/com/jcrew/util/Util.java
@@ -22,8 +22,8 @@ import java.util.*;
 public class Util {
     private static final Logger logger = LoggerFactory.getLogger(Util.class);
 
-    public static final int DEFAULT_TIMEOUT = 60;
-    public static final int DEFAULT_TIMEOUT_STEEL = 120;
+    public static final int DEFAULT_TIMEOUT = 180;
+    public static final int DEFAULT_TIMEOUT_STEEL = 180;
     private static final StateHolder stateHolder = StateHolder.getInstance();
     public static final String xpathGetTextLower = "translate(text(), 'ABCDEFGHJIKLMNOPQRSTUVWXYZ','abcdefghjiklmnopqrstuvwxyz')";
 
