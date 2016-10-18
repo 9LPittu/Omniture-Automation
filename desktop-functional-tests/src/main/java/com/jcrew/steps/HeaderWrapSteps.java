cf6d5ec917900c9c216e085bb10c566660df3684
Swapna Reddivari <swapna.reddivari.cft@jcrew.com>
2016-10-18 09:27:16 -0400

    Hamburger Menu Fix

--- desktop-functional-tests/src/main/java/com/jcrew/steps/HeaderWrapSteps.java
+++ desktop-functional-tests/src/main/java/com/jcrew/steps/HeaderWrapSteps.java
@@ -25,10 +25,7 @@ public class HeaderWrapSteps extends DriverFactory {
 
     @When("User opens menu")
     public void user_opens_menu() {
-
-    	//this is a temparory step - hover on any Gender landing link from top navigation
-    	header.hoverOverIcon("gender landing");
-    	
+   	
         header.openMenu();
     }
 
