805a3fc446e14e0df2b1e0e6ce134142e64ec4b6
Swapna Reddivari <swapna.reddivari.cft@jcrew.com>
2016-10-18 11:51:58 -0400

    commented chrome driver location

--- desktop-functional-tests/src/main/java/com/jcrew/utils/DriverFactory.java
+++ desktop-functional-tests/src/main/java/com/jcrew/utils/DriverFactory.java
@@ -73,7 +73,7 @@ public class DriverFactory {
         WebDriver driver = null;
 
         if ("chrome".equals(browser)) {
-        	System.setProperty("webdriver.chrome.driver", "C:" + "\\" + "git" + "\\" + "chromedriver.exe");
+        	//System.setProperty("webdriver.chrome.driver", "C:" + "\\" + "git" + "\\" + "chromedriver.exe");
             driver = new ChromeDriver();
             driver.manage().window().setSize(new Dimension(width, height));
 
