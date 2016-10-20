c5c41635c71dfeac951d9d4a9c048bce98d1ec87
Swapna Reddivari <swapna.reddivari.cft@jcrew.com>
2016-10-20 00:06:28 -0400

    Removed some changes

--- desktop-functional-tests/src/main/java/com/jcrew/utils/DatabasePropertyReader.java
+++ desktop-functional-tests/src/main/java/com/jcrew/utils/DatabasePropertyReader.java
@@ -31,7 +31,7 @@ public class DatabasePropertyReader {
         databaseInput = new FileInputStream("properties/databasequeries.properties");
         databaseProperties.load(databaseInput);
 
-        String execEnvironment = System.getProperty("environment", "gold");
+        String execEnvironment = System.getProperty("environment", "ci");
         String dbSchemaName = databaseProperties.getProperty(execEnvironment + ".schema");
         databaseProperties.setProperty("schema", dbSchemaName);
         
