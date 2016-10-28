2eb0884cd6df41034c93e4ffecf67e32647aa65c
Vithal Bonker <vbonker@csc.com>
2016-10-28 11:07:24 -0400

    Jcrew chrome firxes, Revert in to original changes

--- cucumber/src/main/java/com/jcrew/util/PropertyReader.java
+++ cucumber/src/main/java/com/jcrew/util/PropertyReader.java
@@ -27,7 +27,7 @@ public class PropertyReader {
     }
 
     private void loadProperties() throws IOException {
-        String execEnvironment = System.getProperty("environment", "qa");
+        String execEnvironment = System.getProperty("environment", "ci");
         String execViewport = System.getProperty("viewport", "phantomjs");
         String execUser = System.getProperty("user", "user.1");
         String country = System.getProperty("country", "us");
