4f6d33f645911f8fd4e07b093d4618b0de0f4454
Swapna Reddivari <swapna.reddivari.cft@jcrew.com>
2016-11-08 11:10:44 -0500

    Annotation update

--- desktop-functional-tests/src/main/java/com/jcrew/steps/GenericVerificationSteps.java
+++ desktop-functional-tests/src/main/java/com/jcrew/steps/GenericVerificationSteps.java
@@ -1,14 +1,14 @@
 package com.jcrew.steps;
 
 import com.jcrew.utils.DriverFactory;
-import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
 import org.openqa.selenium.WebDriver;
 
 import static org.junit.Assert.assertTrue;
 
 public class GenericVerificationSteps extends DriverFactory{
     
-	@And("Verify url is ([^\"]*)$")
+	@Then("Verify url is ([^\"]*)$")
     public void verify_sale_landing_url(String url) {
 		WebDriver driver = 	getDriver();
 		String currentURL = driver.getCurrentUrl();
