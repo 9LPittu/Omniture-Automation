2f864ae8e8578c7877f92de09e2e1b333479501a
Vithal Bonker <vbonker@csc.com>
2016-12-09 13:23:38 -0500

    changed item in data file

--- cucumber/src/main/java/com/jcrew/util/Util.java
+++ cucumber/src/main/java/com/jcrew/util/Util.java
@@ -31,7 +31,7 @@ public class Util {
     public static final String DOWN = "down";
     
     public static String getEnvironment(){
-    	String environment = System.getProperty("environment", "steel");
+    	String environment = System.getProperty("environment", "ci");
     	return environment;
     }
 
