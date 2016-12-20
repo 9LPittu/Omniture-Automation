7527bb65645a6f247f0858e82645d9efe6a02342
Vithal Bonker <vbonker@csc.com>
2016-12-20 06:22:04 -0500

    Added code for classic gift card

--- desktop-functional-tests/src/main/java/com/jcrew/steps/GiftCardSteps.java
+++ desktop-functional-tests/src/main/java/com/jcrew/steps/GiftCardSteps.java
@@ -3,6 +3,7 @@ package com.jcrew.steps;
 import com.jcrew.page.GiftCards;
 import com.jcrew.utils.DriverFactory;
 import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 
 public class GiftCardSteps extends DriverFactory {
@@ -18,4 +19,39 @@ public class GiftCardSteps extends DriverFactory {
     public void verify_gift_card_page() {
     	giftCards.isDisplayed();
     }
+    
+    @When("^User selects \"([^\"]*)\" as card type$")
+    public void user_selects_card_type(String cardType){
+    	giftCards.selectCardType(cardType);
+    }  
+    
+    @And("^User chooses gift amount of ([^\"]*) value$")
+    public void user_chooses_gift_amount(String giftAmount){
+    	giftCards.selectGiftAmount(giftAmount);
+    }
+    
+    @And("^User enters ([^\"]*) as Sender Name$")
+    public void user_enters_sender_name(String senderName){
+    	giftCards.enterSenderName(senderName);
+    }
+    
+    @And("^User enters ([^\"]*) as Recipient Name$")
+    public void user_enters_recipient_name(String recipientName){
+    	giftCards.enterRecipientName(recipientName);
+    }
+    
+    @And("^User enters ([^\"]*) as Recipient Email Address$")
+    public void user_enters_recipient_email_address(String recipientEmailAddress){
+    	giftCards.enterRecipientEmailAddress(recipientEmailAddress);
+    }
+    
+    @And("^User enters ([^\"]*) as gift message in ([^\"]*)$")
+    public void user_enters_gift_message(String message, String fieldName){
+    	giftCards.enterGiftMessage(message, fieldName);
+    }
+    
+    @And("^User clicks Add to Bag on gift cards page$")
+    public void user_clicks_add_to_bag(){
+    	giftCards.clickAddtoBag();
+    }        
 }
