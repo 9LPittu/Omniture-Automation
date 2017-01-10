c7f7345628e7108cd5bac0e2dccdd29f8fb9f37b
Vithal Bonker <vbonker@csc.com>
2017-01-10 12:22:38 -0500

    reverted the jenkins path

--- desktop-functional-tests/src/main/java/com/jcrew/utils/E2EPropertyReader.java
+++ desktop-functional-tests/src/main/java/com/jcrew/utils/E2EPropertyReader.java
@@ -3,7 +3,6 @@ package com.jcrew.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
