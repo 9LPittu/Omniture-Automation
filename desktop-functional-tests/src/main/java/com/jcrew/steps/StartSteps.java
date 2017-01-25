6e2ec31574b86fe632af241afdf9fc78b87a9d41
Swapna Reddivari <swapna.reddivari.cft@jcrew.com>
2017-01-25 12:04:04 -0500

    Added wait time

--- desktop-functional-tests/src/main/java/com/jcrew/steps/StartSteps.java
+++ desktop-functional-tests/src/main/java/com/jcrew/steps/StartSteps.java
@@ -204,6 +204,7 @@ public class StartSteps {
     		String url = reader.getProperty("url");
     		String domain = url.replace("https://", "");
             driver.get(url + "/404");
+            Util.wait(15000);
             JavascriptExecutor jse = (JavascriptExecutor) driver;
             jse.executeScript("document.cookie=\"x-origin=sidecar_render;path=/;domain=" + domain + ";expires=new Date().setDate(new Date().getDate() + 1) \"");
             logger.info("Setting sidecar cookie as: {}", "document.cookie=\"x-origin=sidecar_render;path=/;domain=" + domain + ";expires=new Date().setDate(new Date().getDate() + 1) \"");
