fe5622b9ae38e59f0846d20c26ddd6a33dd5f132
Vithal Bonker <vbonker@csc.com>
2017-02-13 08:52:20 -0500

    deleted unnecessary imports

--- desktop-functional-tests/src/main/java/com/jcrew/page/CheckoutBillingPayment.java
+++ desktop-functional-tests/src/main/java/com/jcrew/page/CheckoutBillingPayment.java
@@ -10,8 +10,6 @@ import com.jcrew.utils.Util;
 import java.util.List;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
