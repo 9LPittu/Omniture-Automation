a9f82b5db4aab5cc983d2f33f999374c964995b2
ngarcia@sawyereffect.com <n4d14G4rc14>
2017-03-10 09:16:30 -0500

    moving new ShoppableTray to jcrew folder

--- desktop-functional-tests/src/test/java/com/jcrew/runner/regression/full/ShoppableTrayRunnerTest.java
+++ desktop-functional-tests/src/test/java/com/jcrew/runner/regression/full/ShoppableTrayRunnerTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "features/regression/ShoppableTray",
+        features = "features/jcrew/regression/ShoppableTray",
         tags = {"@ShoppableTray"},
         glue = {"com.jcrew.steps"},
         format = {"json:target/cucumber-shoppabletrayfeatures.json",
