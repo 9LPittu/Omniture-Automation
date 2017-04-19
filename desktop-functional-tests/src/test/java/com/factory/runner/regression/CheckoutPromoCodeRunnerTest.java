fa8dfb4e5d676e58c3511671d9de4f8a3edd3c3a
ngarcia@sawyereffect.com <n4d14G4rc14>
2017-04-19 15:23:28 -0400

    runners

---
+++ desktop-functional-tests/src/test/java/com/factory/runner/regression/CheckoutPromoCodeRunnerTest.java
@@ -0,0 +1,19 @@
+package com.factory.runner.regression;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"features/factory/regression/checkout/ShoppingBag"},
+        tags = {"@PromoCode"},
+        glue = {"com.jcrew.steps"},
+        format = {
+                "json:target/cucumber-fa-regression-promocode.json",
+                "pretty",
+                "html:target/cucumber/fa-regression-promocode"
+        })
+
+public class CheckoutPromoCodeRunnerTest {
+}
