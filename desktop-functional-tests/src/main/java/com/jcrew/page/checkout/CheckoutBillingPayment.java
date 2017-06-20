587baa9d1eb29df9ef06f4a83ab6c9255ac47e5f
Vithal Bonker <vbonker@csc.com>
2017-06-20 09:50:55 -0400

    script fixes

--- desktop-functional-tests/src/main/java/com/jcrew/page/checkout/CheckoutBillingPayment.java
+++ desktop-functional-tests/src/main/java/com/jcrew/page/checkout/CheckoutBillingPayment.java
@@ -39,7 +39,7 @@ public class CheckoutBillingPayment extends Checkout {
     private WebElement expirationYear;    
     @FindBy(id = "securityCode")
     private WebElement securityCode;
-    @FindBy(id = "billing-options-submit")
+    @FindBy(id = "button-submit")
     private WebElement submitButton;    
     @FindBy(id = "main_cont")
     private WebElement main_cont;
@@ -151,6 +151,9 @@ public class CheckoutBillingPayment extends Checkout {
     }
     
     public void submitPayment() {
+    	Util.waitForPageFullyLoaded(driver);
+    	Util.waitLoadingBar(driver);
+    	
         String currentURl = driver.getCurrentUrl();
         submitButton.click();
 
