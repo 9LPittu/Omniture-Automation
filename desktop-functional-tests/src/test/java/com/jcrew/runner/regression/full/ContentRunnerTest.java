5d3f7157168706da2fece3bcbbb84799f974b85d
Venkatakrishna Challa <venkatakrishna-c@HCL.COM>
2017-11-10 11:25:36 +0530

    content regression

--- desktop-functional-tests/src/test/java/com/jcrew/runner/regression/full/ContentRunnerTest.java
+++ desktop-functional-tests/src/test/java/com/jcrew/runner/regression/full/ContentRunnerTest.java
@@ -1,7 +1,14 @@
 package com.jcrew.runner.regression.full;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+
+import com.jcrew.listeners.Reporter;
 
 
 @RunWith(Cucumber.class)
@@ -9,14 +16,25 @@ import org.junit.runner.RunWith;
         features = {"features/jcrew/regression/Content"},
         tags = {"@JcrewContentTesting"},
         glue = {"com.jcrew.steps"},
-        format = {
-                "json:target/cucumber-Content.json",
+        		plugin = {"com.jcrew.listners.ExtentCucumberFormatter:"}
+        /*format = {
+                "json:target/cucumber-Content_Jcrew.json",
                 "pretty",
-                "html:target/cucumber/Content"
-        }
+                "html:target/cucumber/Content_Jcrew"
+        }*/
 )
 
 public class ContentRunnerTest {
+	@BeforeClass
+    public static void setup() {
+        Reporter.setReportsConfig("JCFAR");
+    }
+
+    @AfterClass
+    public static void teardown() {
+        Reporter.loadXMLConfig(new File(System.getProperty("user.dir") + "/properties/reports-config.xml"));
+        Reporter.setSystemInfo("user", System.getProperty("user.name"));
+    }
 	
 }
 
