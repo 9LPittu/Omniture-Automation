02f2c3d060d9dbe15917a995967f862e516054c9
VenkatS <venkatanarayana.savilla.cft@jcrew.com>
2018-01-03 23:22:35 +0530

    Factory omiture scripts updated

---
+++ desktop-functional-tests/src/test/java/com/factory/runner/omniture/OmnitureRunnerTest.java
@@ -0,0 +1,36 @@
+package com.factory.runner.omniture;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+import java.io.File;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+
+import com.jcrew.listners.Reporter;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"features/factory/omniture"},
+        tags = {"@s_omnitureArray"},
+        glue = {"com.jcrew.steps"},
+        plugin = {"com.jcrew.listners.ExtentCucumberFormatter:"}
+)
+
+public class OmnitureRunnerTest {
+	@BeforeClass
+    public static void setup() {
+        Reporter.setReportsConfig("JCFAR");
+    }
+
+    @AfterClass
+    public static void teardown() {
+        Reporter.loadXMLConfig(new File(System.getProperty("user.dir") + "/properties/reports-config.xml"));
+        Reporter.setSystemInfo("user", System.getProperty("user.name"));
+    }
+
+
+}
