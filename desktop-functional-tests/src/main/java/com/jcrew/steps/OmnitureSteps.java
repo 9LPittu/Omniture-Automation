8e7082a127fd3ae29c2c82484075f12ad338d094
venkat <laxmman.perumandla@jcrew.com>
2018-01-25 19:12:56 +0530

    omniture fixes

--- desktop-functional-tests/src/main/java/com/jcrew/steps/OmnitureSteps.java
+++ desktop-functional-tests/src/main/java/com/jcrew/steps/OmnitureSteps.java
@@ -12,38 +12,51 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import com.jcrew.listners.Reporter;
 
 public class OmnitureSteps extends DriverFactory{
 	private TestDataReader testDataReader = TestDataReader.getTestDataReader();
 	
-	
+	public String previousOmni="";
 	@Then("^Verify omniture variables have values$")
     public void verify_omniture_variables() throws InterruptedException {
         TestDataReader testDataReader =  TestDataReader.getTestDataReader();
-
         String arrOmnitureVariables[] = testDataReader.getDataArray("omniture.variables");
         List<String> omnitureVariables = Arrays.asList(arrOmnitureVariables);
 
         String emptyVariables = "";
         boolean isblank=false;
+        boolean isEqual=false;  
         for (String omnitureVariable:omnitureVariables) {
-            String omnitureValue = Util.getPageVariableValue(getDriver(), omnitureVariable);
-            if (omnitureValue.isEmpty()) {
+            String omnitureValue = Util.getPageVariableValue(getDriver(), omnitureVariable); 
+            if (!omnitureValue.isEmpty() && omnitureValue.equals(previousOmni)) 
+            {
+                emptyVariables = emptyVariables + omnitureVariable + ";";
+                isEqual = true;
+                break;
+            }
+            else if (omnitureValue.isEmpty() || omnitureValue.equals(previousOmni)) 
+            {
                 emptyVariables = emptyVariables + omnitureVariable + ";";
                 isblank = true;
             }
             else
+            {
             	Reporter.addStepLog(" omniture Value for the variable "+omnitureVariable+" is.. "+omnitureValue);
-              
-        }
-
+            	System.out.println(" omniture Value for the variable "+omnitureVariable+" is.. "+omnitureValue);
+            }
+            previousOmni= omnitureValue;
+        }        
         if (isblank)
-           // throw new WebDriverException("Omniture variables " + emptyVariables + " does not have a value");
-        	Reporter.addStepLog("Omniture variables " + emptyVariables + " does not have a value");
+        {
+        	throw new WebDriverException("Omniture variables " + emptyVariables + " does not have a value");
+        }
+        if (isEqual) {
+        	throw new WebDriverException("Previous category Omniture value and current category Omniture value is equal");
+   		    }
     }
-	
-	
+
 	@Then("Verify correct value is displayed for the omniture variable ([^\"]*)$")
 	public void verify_omniture_variable_value(String variable) throws InterruptedException {
 		String expectedValue = testDataReader.getData(variable+".value");
