6e26a3231e3198f66ea412881d5afe242a3752bd
Admin <admin@770-lg-20154.jcrew.com>
2018-08-29 16:01:05 +0530

    loyalty tests 29Aug

--- sidecar-automation-js/tests/specs/desktop/loyalty/loyaltyfaqpagetest.js
+++ sidecar-automation-js/tests/specs/desktop/loyalty/loyaltyfaqpagetest.js
@@ -1,8 +1,7 @@
 import { driver } from '../../../helpers';
 import { load } from '../../../pageObjects/jcrewdesktoppageobj';
 import { globals } from '../../../jestJcrewQaConfig';
-import { creditcard } from '../../../testdata/jcrewTestData';
-import { guestuser } from '../../../testdata/jcrewTestData';
+
 
 const each = require('jest-each')
 const { Builder, By, Key, until } = require('selenium-webdriver')
@@ -15,6 +14,31 @@ beforeAll(async () => {
 
 
      test('Verify Loyalty footer Faq links', async () => {
-      
-    })
 
+      await driver.sleep(2000)
+      await driver.executeScript('window.scrollTo(0, 50000)')
+      await driver.sleep(2000)
+      await driver.executeScript("arguments[0].scrollIntoView(true);",driver.findElement(By.xpath("//h6[text()='J.CREW REWARDS']")));
+      await driver.sleep(2000)
+      const jcrewrewards = await driver.findElement(By.xpath("//h6[text()='J.CREW REWARDS']"));
+      expect(jcrewrewards).toBeTruthy()
+      //verify sign up now link
+      const signUpNow = await driver.findElement(By.xpath("//div[text()='Sign Up Now']"));
+      expect(signUpNow).toBeTruthy()
+      signUpNow.click()
+      await driver.sleep(5000)
+      let loginurl = await driver.getCurrentUrl();
+      expect(loginurl.includes("/r/login")).toBeTruthy()
+      console.log("user navigated to login page");
+      await driver.sleep(2000)
+      driver.navigate().back()
+      await driver.sleep(2000)
+      const FAQs = await driver.findElement(By.xpath("//div[text()='FAQs']"));
+      expect(FAQs).toBeTruthy()
+      FAQs.click()
+      await driver.sleep(4000)
+      let faqurl = await driver.getCurrentUrl();
+      expect(faqurl.includes("/l/rewards")).toBeTruthy()
+      console.log("user navigated to FAQs page");
+      await driver.sleep(2000)
+    })
--- sidecar-automation-js/tests/specs/desktop/loyalty/loyaltylightboxtest.js
+++ sidecar-automation-js/tests/specs/desktop/loyalty/loyaltylightboxtest.js
@@ -1,8 +1,7 @@
 import { driver } from '../../../helpers';
 import { load } from '../../../pageObjects/jcrewdesktoppageobj';
 import { globals } from '../../../jestJcrewQaConfig';
-import { creditcard } from '../../../testdata/jcrewTestData';
-import { guestuser } from '../../../testdata/jcrewTestData';
+
 
 const each = require('jest-each')
 const { Builder, By, Key, until } = require('selenium-webdriver')
@@ -16,18 +15,24 @@ beforeAll(async () => {
 
 
      test('Verify Loyalty Lightbox is comming every time and able to login via loyalty Light Box', async () => {
-   
       var x = Math.floor((Math.random() * 99900000) + 1);
       let userName = "LoyaltyTest"+x
       let email = userName+x+"@gmail.com"
-
       await driver.findElement(By.xpath("//*[@id='global__email-capture']/section/div[1]/form/fieldset/input[1]")).sendKeys(email)
+      driver.sleep(1000)
       await driver.findElement(By.xpath("//*[@id='global__email-capture']/section/div[1]/form/fieldset/button/span[2]")).click();
-      await driver.findElement(By.xpath("//*[@id='global__email-capture']/section/div[2]/form/input")).click();
+      driver.sleep(3000)
+      let currentUrl = await driver.getCurrentUrl();
+    if (currentUrl.indexOf("factory.jcrew.com") > -1) {
+      await driver.findElement(By.xpath("//input[@placeholder='CREATE YOUR PASSWORD']")).sendKeys("123456ab");
+    }else {
+        await driver.findElement(By.xpath("//input[@placeholder='Create Your Password']")).sendKeys("123456ab");
+    }
+      driver.sleep(1000)
+      await driver.findElement(By.xpath("//*[text()='Join Now']")).click();
+      driver.sleep(10000)
       console.log ("Loyalty User created from lightbox >> " + email)
-      
-
-      
-
+      const myaccount = await driver.findElement(By.xpath("//span[text()='My Account']"))
+      expect(myaccount).toBeTruthy()
+      console.log("Logged in to application using loyalty lightbox successfully")
     })
-
